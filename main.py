d9183fba3c3e84bb3a1366f4b9ee5018c3a678b2
truonganhtuan290514 <truonganhtuan290514@gmail.com>
2026-08-19 20:39:12 +0700

    Them diem Anh

--- main.py
+++ main.py
@@ -6,3 +6,9 @@ diem_tb = (toan + van) / 2
 
 print("Ten hoc sinh:", ten)
 print("Diem trung binh:", diem_tb)
+
+anh = float(input("Nhap diem Anh: "))
+
+diem_tb = (toan + van + anh) / 3
+
+print("Diem trung binh 3 mon:", diem_tb)
