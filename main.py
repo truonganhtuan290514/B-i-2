bb1ed3fff4f15714cec9ca35c96dae51d03f5229
truonganhtuan290514 <truonganhtuan290514@gmail.com>
2026-08-19 20:17:19 +0700

    Tao chuong trinh tinh diem

---
+++ main.py
@@ -0,0 +1,8 @@
+ten = input("Nhap ten hoc sinh: ")
+toan = float(input("Nhap diem Toan: "))
+van = float(input("Nhap diem Van: "))
+
+diem_tb = (toan + van) / 2
+
+print("Ten hoc sinh:", ten)
+print("Diem trung binh:", diem_tb)
